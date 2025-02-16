a5240005eea8b29bc15e6f93eb9ec71653ae9a3a
Cezar Drumea <cdrumea.griddynamics@gmail.com>
2025-02-16 13:03:14 +0200

    chore: change folder generation logic

--- src/components/selects/FolderSelect.tsx
+++ src/components/selects/FolderSelect.tsx
@@ -1,13 +1,19 @@
+import getFolders from '../../utils/getFolders';
 import ArrowIcon from '../icons/ArrowIcon';
 import FolderIcon from '../icons/FolderIcon';
 
 const FolderSelect = ({ className }: { className?: string }) => {
+  const folders = getFolders();
+
   return (
     <label className={`${className} relative`}>
       <FolderIcon className='absolute top-1/2 transform -translate-y-1/2 left-3 pointer-events-none' />
       <select className='h-8 text-sm text-secondary-100 pl-9 pr-9 border-1 border-solid border-secondary-20 hover:border-secondary-100 rounded-md appearance-none cursor-pointer duration-200'>
-        <option value='Your folder'>Your folder</option>
-        <option value='New folder'>New folder</option>
+        {folders.map((folder) => (
+          <option key={folder.id} value={folder.id}>
+            {folder.name}
+          </option>
+        ))}
       </select>
       <ArrowIcon className='absolute top-1/2 transform -translate-y-1/2 right-3 pointer-events-none text-secondary-60' />
     </label>
--- src/utils/getFolders.ts
+++ src/utils/getFolders.ts
@@ -1,11 +1,18 @@
 import { FOLDERS_QUANTITY } from '../data';
 import { FolderIdType } from '../types/file';
 
+const getFolderName = (idNum: number) => {
+  if (idNum === 0) return 'Your folder';
+  if (idNum === 1) return 'New folder';
+
+  return `New folder (${idNum + 1})`;
+};
+
 const getFolders = () => {
   return [...new Array(FOLDERS_QUANTITY)].map((_, i) => {
     return {
       id: `folder${i}`,
-      name: `${i === 0 ? 'Your folder' : 'New folder'}`,
+      name: getFolderName(i),
     } as {
       id: FolderIdType;
       name: string;
